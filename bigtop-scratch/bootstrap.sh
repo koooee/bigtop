4879ea003e3bd067002d77675175ed1ff63d5b88
koooee <nick@winthropgaming.com>
2012-10-31 02:09:30 -0400

    added package dependencies for centos and redhat

--- bigtop-scratch/bootstrap.sh
+++ bigtop-scratch/bootstrap.sh
@@ -1,45 +1,41 @@
-#!/bin/bash -e
-
 # Script to bootstrap a dev environment for Apache BigTop
+# Script should be run via: source bootstrap.sh
+APACHE_FORREST=http://archive.apache.org/dist/forrest/0.8/apache-forrest-0.8.tar.gz
 
 if [ "$(id -u)" != "0" ]; then
     echo "Run this script as root or sudo";
     exit;
 fi
 
-function install_apache_forrest {
+function install_apache_forrest () {
     
     # Download it
     pushd /opt
-    wget -O - http://archive.apache.org/dist/forrest/0.8/apache-forrest-0.8.tar.gz | tar -xz
+    wget -O - $1 | tar -xz
     pushd apache-forrest-*
 
     # Set up environment
-    if [ "$(grep -c FORREST_HOME /etc/bashrc)" = 0]; then
+    if [ "$(grep -c FORREST_HOME /etc/bashrc)" -eq "0" ]; then
+	echo "Adding forrest to profile"
 	echo -e "\nexport FORREST_HOME=$PWD" >> /etc/bashrc
 	echo -e "\nexport PATH=$PATH:$FORREST_HOME/bin" >> /etc/bashrc
+	source /etc/bashrc
     fi
-
+    
     # Build it
     pushd main
     ./build.sh
     dirs -c
 }
 
-yum install git java-1.6.0-openjdk-devel java-1.6.0-openjdk-devel maven subversion gcc gcc-c++ make fuse
-install_apache_forrest
-exit;    
-
 # Poorly attempt to determine the distribution
 DISTRO=$(cat /proc/version | sed -e 's/ //g' | tr '[:upper:]' '[:lower:]' | egrep -o "redhat|centos|ubuntu|debian|suse" | head -n1)
-exit
 case $DISTRO in
     centos)
 	;&
     redhat)
-	yum install git java-1.6.0-openjdk-devel java-1.6.0-openjdk-devel maven subversion gcc gcc-c++ make fuse
-
-
+        yum -y install git java-1.6.0-openjdk-devel java-1.6.0-openjdk-devel maven subversion gcc gcc-c++ make fuse fuse-devel lzo-devel sharutils ssl-devel
+	install_apache_forrest $APACHE_FORREST;;
     ubuntu)
 	echo "ubuntu";;
     debian)
@@ -48,7 +44,5 @@ case $DISTRO in
 	echo "suse";;
     *)
 	echo "Couldn't determine your distribution."
-	exit;;
 esac
 
-exit
