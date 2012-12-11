15d547666735fb1346e6fb7ba085f6008fc73629
koooee <nick@winthropgaming.com>
2012-12-11 03:01:57 -0500

    reverted back to custom ant, solr needs ant1.8

--- bigtop-scratch/bootstrap.sh
+++ bigtop-scratch/bootstrap.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -e
 # Script to bootstrap a dev environment for Apache BigTop
-# Script should be run via: source bootstrap.sh
+
 APACHE_FORREST=http://archive.apache.org/dist/forrest/0.8/apache-forrest-0.8.tar.gz
 MAVEN=http://mirror.reverse.net/pub/apache/maven/maven-3/3.0.4/binaries/apache-maven-3.0.4-bin.tar.gz
 PROTOBUF=http://protobuf.googlecode.com/files/protobuf-2.4.1.tar.gz
 ANT=http://apache.mirrors.lucidnetworks.net//ant/binaries/apache-ant-1.8.4-bin.tar.gz
 REAL_USER=$(who am i | cut -d" " -f1)
-PROFILE=/etc/bashrc
+PROFILE=bigtop-env.sh
 NEW_PATH=$PATH
 # Check if we have super powers
 if [ "$(id -u)" != "0" ]; then
@@ -66,6 +66,7 @@ function install_protobuf () {
     dirs -c
 }
 
+# Solr Requres Ant1.8 and yum only has 1.7
 function install_ant () {
     # Download it
     pushd /opt
@@ -86,10 +87,11 @@ case $DISTRO in
     centos)
 	;&
     redhat)
-    yum -y install git java-1.6.0-openjdk-devel java-1.6.0-openjdk-devel ant-* subversion gcc gcc-c++ make cmake javacc fuse fuse-devel lzo-devel sharutils rpm-build automake libtool redhat-rpm-config openssl-devel zlib-devel python-devel libxml2-devel libxslt-devel cyrus-sasl-devel sqlite-devel mysql-devel openldap-devel createrepo asciidoc xmlto python-setuptools
+    yum -y install git java-1.6.0-openjdk-devel java-1.6.0-openjdk-devel subversion gcc gcc-c++ make cmake javacc fuse fuse-devel lzo-devel sharutils rpm-build automake libtool redhat-rpm-config openssl-devel zlib-devel python-devel libxml2-devel libxslt-devel cyrus-sasl-devel sqlite-devel mysql-devel openldap-devel createrepo asciidoc xmlto python-setuptools
     install_apache_forrest $APACHE_FORREST
     install_maven $MAVEN
-    install_protobuf $PROTOBUF;;
+    install_protobuf $PROTOBUF
+    install_ant $ANT;;
     ubuntu)
 	echo "ubuntu";;
     debian)
